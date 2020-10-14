1c2715fdd5a5c4ba1cd42827e33fecaa1ac3c3f1
Kelly Shull <kelly.t.shull@gmail.com>
2020-10-14 14:04:25 -0700

    parsed the data

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -9,37 +9,51 @@ function loadChart() {
   }
 
 // Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 500;
+  var svgWidth = 960;
+  var svgHeight = 500;
 
-// Define the chart's margins as an object
-var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
-};
+  // Define the chart's margins as an object
+  var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+  };
 
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+  // Define dimensions of the chart area
+  var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+  var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+  
+  // Select body, append SVG area to it, and set the dimensions
+  var svg = d3.select("#scatter")
+            .append("svg")
+            .attr("height", svgHeight)
+            .attr("width", svgWidth);
 
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3.select("#scatter")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+  // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+  var chartGroup = svg.append("g")
+                  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+  // Load data from data.csv
+  d3.csv("./assets/data/data.csv").then(healthData => {
 
-// Load data from data.csv
-d3.csv("./assets/data/data.csv").then(healthData => {
+     
 
-    console.log(healthData);
+    // parse data 
+    healthData.forEach(data => {
+      // x-axis data
+      data.poverty = +data.poverty;
+      data.income = +data.income;
+      data.age = +data.age;
+      // y-axis data
+      data.healthcare = +data.healthcare;
+      data.smokes = +data.smokes;
+      data.obesity = +data.obesity;
+    });
 
-}).catch(error => console.log(error));
+    // console.log(healthData);
+
+  }).catch(error => console.log(error));
 
 }
 
