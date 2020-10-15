cb08f9e8d72c3f229cab464d7b55eef52ecd9252
Kelly Shull <kelly.t.shull@gmail.com>
2020-10-15 16:59:20 -0700

    labels group added to document

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -5,10 +5,10 @@ function loadChart() {
 
   // if there is already a svg container on page, remove it and reload chart
   if (!svgArea.empty()) {
-        svgArea.remove();
+    svgArea.remove();
   }
 
-// Define SVG area dimensions
+  // Define SVG area dimensions
   var svgWidth = 960;
   var svgHeight = 500;
 
@@ -16,66 +16,67 @@ function loadChart() {
   var chartMargin = {
     top: 30,
     right: 30,
-    bottom: 30,
+    bottom: 80,
     left: 30
   };
 
   // Define dimensions of the chart area
   var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
   var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-  
+
   // Select body, append SVG area to it, and set the dimensions
   var svg = d3.select("#scatter")
-            .append("svg")
-            .attr("height", svgHeight)
-            .attr("width", svgWidth);
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
 
   // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
   var chartGroup = svg.append("g")
-                  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
-  
-  
-var chosenXAxis = "age";
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
-// function used for updating x-scale var upon click on axis label
-function xScale(healthData, chosenXAxis) {
-  // create scales
-  var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.8,
+
+  var chosenXAxis = "age";
+  // var chosenYAxis = "smokes";
+
+  // function used for updating x-scale var upon click on axis label
+  function xScale(healthData, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(healthData, d => d[chosenXAxis]) * 0.8,
       d3.max(healthData, d => d[chosenXAxis]) * 1.2
-    ])
-    .range([0, chartWidth]);
+      ])
+      .range([0, chartWidth]);
 
-  return xLinearScale;
+    return xLinearScale;
 
-}
+  }
 
-// function used for updating xAxis var upon click on axis label
-function renderAxes(newXScale, xAxis) {
-  var bottomAxis = d3.axisBottom(newXScale);
+  // function used for updating xAxis var upon click on axis label
+  function renderXAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
 
-  xAxis.transition()
-    .duration(1000)
-    .call(bottomAxis);
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
 
-  return xAxis;
-}
+    return xAxis;
+  }
 
-// function used for updating circles group with a transition to
-// new circles
-function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+  // function used for updating circles group with a transition to
+  // new circles
+  function renderCircles(circlesGroup, newXScale, chosenXAxis) {
 
-  circlesGroup.transition()
-    .duration(1000)
-    .attr("cx", d => newXScale(d[chosenXAxis]));
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenXAxis]));
 
-  return circlesGroup;
-}
+    return circlesGroup;
+  }
 
   // Load data from data.csv
   d3.csv("./assets/data/data.csv").then(healthData => {
 
-     
+
 
     // parse data 
     healthData.forEach(data => {
@@ -97,35 +98,53 @@ function renderCircles(circlesGroup, newXScale, chosenXAxis) {
     //             .range([0, chartWidth]);
     var xLinearScale = xScale(healthData, chosenXAxis)
     var yLinearScale = d3.scaleLinear()
-                .domain([d3.min(healthData, d => d.smokes) - 5, d3.max(healthData, d => d.smokes) + 5 ])
-                .range([chartHeight, 0]);
-    
+      .domain([d3.min(healthData, d => d.smokes) - 5, d3.max(healthData, d => d.smokes) + 5])
+      .range([chartHeight, 0]);
+
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    chartGroup.append("g")
+    var xAxis = chartGroup.append("g")
       .attr("transform", `translate(0, ${chartHeight})`)
       .call(bottomAxis);
 
-    chartGroup.append("g")
+    var yAxis = chartGroup.append("g")
       .call(leftAxis);
-    
+
     // create circles 
     var circleGroup = chartGroup.selectAll("circle")
-                      .data(healthData)
-                      .join("circle")
-                      .attr("cx", d => xLinearScale(d.age))
-                      .attr("cy", d => yLinearScale(d.smokes))
-                      .attr("r", 15)
-                      .attr("class", "stateCircle");
-    
+      .data(healthData)
+      .join("circle")
+      .attr("cx", d => xLinearScale(d.age))
+      .attr("cy", d => yLinearScale(d.smokes))
+      .attr("r", 15)
+      .attr("class", "stateCircle");
+
     var textGroup = chartGroup.selectAll("text.stateText")
-                    .data(healthData)
-                    .join("text")
-                    .attr("x", d => xLinearScale(d.age))
-                    .attr("y", d => yLinearScale(d.smokes)+5)
-                    .text(d => (d.abbr))
-                    .attr("class", "stateText");
+      .data(healthData)
+      .join("text")
+      .attr("x", d => xLinearScale(d.age))
+      .attr("y", d => yLinearScale(d.smokes) + 5)
+      .text(d => (d.abbr))
+      .attr("class", "stateText");
+
+    // Create group for two x-axis labels
+    var xLabelsGroup = chartGroup.append("g")
+      .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + 20})`);
+
+    var ageLabel = xLabelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .attr("value", "age") // value to grab for event listener
+      .classed("active", true)
+      .text("Age (Median)");
+
+    var povertyLabel = xLabelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 40)
+      .attr("value", "poverty") // value to grab for event listener
+      .classed("inactive", true)
+      .text("Poverty (%)");
 
   }).catch(error => console.log(error));
 
